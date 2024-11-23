a346233862dfcaae6f78318f4816371bbd004a42
hasan <hasan@VICTUS>
2024-11-23 15:35:12 +0500

    Add project files.

---
+++ Data structure project/AVL.h
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+//avl node struct
+template <typename t>
+struct AVL_node {
+	t data;
+	AVL_node* left;
+	AVL_node* right;
+
+	//constructor to initialise avl node
+	
+	AVL_node(t d = 0, AVL_node* l = nullptr, AVL_node* r = nullptr) :data(d), left(l), right(r)
+	{
+
+	}
+};
+
+template <typename t>
+class AVL_tree {
+public:
+	AVL_node<t>* root;
+
+	void insert(t val)
+	{
+
+	}
+
+	void search(t val)
+	{
+
+	}
+
+	void delete(t val)
+	{
+
+	}
+	
+};
---
+++ Data structure project/Btree.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+//btree node struct
+template <typename t>
+struct Btree_node {
+
+	int order			//order of the btree
+	t*keys				//array of teh keys
+	Btree_node*children //array to keep track of the child pointers
+
+	
+};
