59db8c4d0da2035e10251242b795140c177ca200
Hasan Naveed <hasannaveed47@gmail.com>
2024-11-24 00:10:43 +0500

    avl tree class added

--- Data structure project/Source.cpp
+++ Data structure project/Source.cpp
@@ -1,10 +1,26 @@
 #include <iostream>
+#include "AVL.h"
 using namespace std;
 
 
 int main() {
 
+	//testing the avl tree
 
+	AVL_tree<int> tree;
+	tree.insert(1);
+	tree.insert(2);
+	tree.insert(3);
+	tree.insert(4);
+	tree.insert(5);
+	tree.insert(6);
+	tree.insert(7);
+	tree.insert(8);
+	tree.insert(9);
+	tree.insert(10);
+	tree.deletion(10);
+	tree.deletion(6);
+	tree.inorder();
 
 	return 0;
 }
